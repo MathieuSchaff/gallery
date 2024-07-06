46804a0ccea8761bf0949bee6ef2a8c18000b382
schaff <schaff.mathieu93@gmail.com>
2024-07-06 05:15:31 +0200

    scaffold ui

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,31 @@
 import Link from "next/link";
 
+const mockUrls = [
+  "https://utfs.io/f/a5d6752c-601a-41c6-a79e-3c5e5325d10e-5s19bp.jpg",
+  "https://utfs.io/f/52677835-2d83-4952-9f14-9b079b604c5b-1r1j2k.JPG",
+  "https://utfs.io/f/95b56846-d940-4b11-a816-8575d8a4c112-d8rnt2.jpg",
+];
+
+const mocksImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello gallery in progress
+    <main className="">
+      <div className="-m-2 flex flex-wrap justify-around">
+        {mocksImages.map((image, index) => (
+          <div key={index} className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
+            <div className="aspect-square overflow-hidden">
+              <img
+                src={image.url}
+                alt={image.url}
+                className="h-full w-full object-cover"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
