5367a9fcd4548a6b58bfdc4b5dc03523c06f94f5
schaff <schaff.mathieu93@gmail.com>
2024-08-10 04:52:40 +0200

    used auth for home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
 import Link from "next/link";
 import { db } from "../server/db/index";
+import { SignedIn, SignedOut, SignIn } from "@clerk/nextjs";
 export const dynamic = "force-dynamic";
-
-export default async function HomePage() {
+export async function Images() {
   const images = await db.query.images.findMany();
-  console.log(images);
   return (
-    <main className="flex flex-wrap">
+    <>
       {images.map((image, index) => (
         <div key={index} className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
           <div className="aspect-square overflow-hidden">
@@ -18,6 +17,16 @@ export default async function HomePage() {
           </div>
         </div>
       ))}
+    </>
+  );
+}
+export default async function HomePage() {
+  return (
+    <main className="flex flex-wrap">
+      <SignedOut>Please sign in</SignedOut>
+      <SignedIn>
+        <Images />
+      </SignedIn>
     </main>
   );
 }
