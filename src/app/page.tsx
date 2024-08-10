2bf2c546c1aa81244f7d98800735a18ebefa21fd
schaff <schaff.mathieu93@gmail.com>
2024-08-10 02:09:32 +0200

    force dynamic ( next js feature)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
-
+import { db } from "../server/db/index";
+export const dynamic = "force-dynamic";
 const mockUrls = [
   "https://utfs.io/f/a5d6752c-601a-41c6-a79e-3c5e5325d10e-5s19bp.jpg",
   "https://utfs.io/f/52677835-2d83-4952-9f14-9b079b604c5b-1r1j2k.JPG",
@@ -10,9 +11,14 @@ const mocksImages = mockUrls.map((url, index) => ({
   id: index + 1,
   url,
 }));
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
   return (
     <main className="flex flex-wrap">
+      {posts.map((post, index) => {
+        return <div key={post.id}>{post.name}</div>;
+      })}
       {mocksImages.map((image, index) => (
         <div key={index} className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
           <div className="aspect-square overflow-hidden">
