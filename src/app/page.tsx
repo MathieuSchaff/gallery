4a233b5f6e0ed9ae3be35657f37cf4dc0fb68433
schaff <schaff.mathieu93@gmail.com>
2024-08-10 03:41:43 +0200

    setup auth with clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,25 +1,13 @@
 import Link from "next/link";
 import { db } from "../server/db/index";
 export const dynamic = "force-dynamic";
-const mockUrls = [
-  "https://utfs.io/f/a5d6752c-601a-41c6-a79e-3c5e5325d10e-5s19bp.jpg",
-  "https://utfs.io/f/52677835-2d83-4952-9f14-9b079b604c5b-1r1j2k.JPG",
-  "https://utfs.io/f/95b56846-d940-4b11-a816-8575d8a4c112-d8rnt2.jpg",
-];
 
-const mocksImages = mockUrls.map((url, index) => ({
-  id: index + 1,
-  url,
-}));
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
-  console.log(posts);
+  const images = await db.query.images.findMany();
+  console.log(images);
   return (
     <main className="flex flex-wrap">
-      {posts.map((post, index) => {
-        return <div key={post.id}>{post.name}</div>;
-      })}
-      {mocksImages.map((image, index) => (
+      {images.map((image, index) => (
         <div key={index} className="w-full p-2 sm:w-1/2 md:w-1/3 lg:w-1/4">
           <div className="aspect-square overflow-hidden">
             <img
