8d664bf386f8baf1273cfd846635315a0081682c
schaff <schaff.mathieu93@gmail.com>
2024-09-03 04:32:07 +0200

    adding button for uploading file to topnav

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,5 +1,9 @@
+"use client";
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
+import { useRouter } from "next/navigation";
+import { UploadButton } from "~/utils/uploadthing";
 export function TopNav() {
+  const router = useRouter();
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Gallery</div>
@@ -8,7 +12,15 @@ export function TopNav() {
           <SignInButton />
         </SignedOut>
         <SignedIn>
-          <UserButton />
+          <div className="flex gap-2">
+            <UploadButton
+              endpoint="imageUploader"
+              onClientUploadComplete={() => {
+                router.refresh();
+              }}
+            />
+            <UserButton />
+          </div>
         </SignedIn>
       </div>
     </nav>
